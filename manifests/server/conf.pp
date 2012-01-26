4a48e31712b20543b8872e284941b84ebc02d7a8
Rudy Gevaert <rudy@webworm.org>
2012-01-26 11:07:32 +0100

    fix local site plugin dir in template

--- manifests/server/conf.pp
+++ manifests/server/conf.pp
@@ -2,7 +2,7 @@
 class mcollective::server::conf {
 
   $plugin_path=$mcollective::params::plugin_path 
-  $site_plugin_path=$mcollective::params::site_plugin_path 
+  $site_plugin_path_root=$mcollective::params::site_plugin_path_root
   file {
     "mcollective-server.cfg":
       content => template('mcollective/server.cfg.erb'),
